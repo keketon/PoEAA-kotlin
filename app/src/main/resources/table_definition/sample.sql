ee69a03cc60efcaa7f92e87fd3a1bd58303aba8f
keketon <mas08rin10yabko@gmail.com>
2021-11-21 21:12:14 +0900

    change schema

--- app/src/main/resources/table_definition/sample.sql
+++ app/src/main/resources/table_definition/sample.sql
@@ -2,39 +2,40 @@ CREATE DATABASE IF NOT EXISTS balance_sheet;
 
 DROP TABLE IF EXISTS balance_sheet.sample;
 CREATE TABLE balance_sheet.sample(
-    id BIGINT NOT NULL,
+    id char(36) NOT NULL,
     PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='sample';
 
 INSERT INTO balance_sheet.sample
-VALUES (1);
+VALUES ('04d88a00-e4b0-4f6e-95ee-1d30d84e2a27');
 
 DROP TABLE IF EXISTS balance_sheet.engineers;
 CREATE TABLE balance_sheet.engineers(
-    id BIGINT NOT NULL,
+    id char(36) NOT NULL,
     name varchar(200) NOT NULL,
+    unit_price BIGINT NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='engineers';
 
 INSERT INTO balance_sheet.engineers
-VALUES (1, 'keketon');
+VALUES ('d6f2ace2-ba0d-4b44-b879-36c2accca949', 'keketon', 1000000);
 
 DROP TABLE IF EXISTS balance_sheet.projects;
 CREATE TABLE balance_sheet.projects(
-    id BIGINT NOT NULL,
+    id char(36) NOT NULL,
     name varchar(200) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='projects';
 
 INSERT INTO balance_sheet.projects
-VALUES (1, 'projectX');
+VALUES ('80811c8f-9332-4ab5-9e82-d563e3a20eb2', 'projectX');
 
 DROP TABLE IF EXISTS balance_sheet.assignments;
 CREATE TABLE balance_sheet.assignments(
-    engineer_id BIGINT NOT NULL,
-    project_id BIGINT NOT NULL,
+    engineer_id char(36) NOT NULL,
+    project_id char(36) NOT NULL,
     commit_percent INT NOT NULL,
     PRIMARY KEY (engineer_id, project_id),
     FOREIGN KEY fk_engineer_id (engineer_id) REFERENCES engineers(id),
@@ -42,4 +43,4 @@ CREATE TABLE balance_sheet.assignments(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='assignments';
 
 INSERT INTO balance_sheet.assignments
-VALUES (1, 1, 100);
+VALUES ('d6f2ace2-ba0d-4b44-b879-36c2accca949', '80811c8f-9332-4ab5-9e82-d563e3a20eb2', 100);
