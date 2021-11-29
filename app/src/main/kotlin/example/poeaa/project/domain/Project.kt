cff36dc98b2e2159f2a0e502dd2da9637365a302
keketon <mas08rin10yabko@gmail.com>
2021-11-29 19:38:49 +0900

    project entities

---
+++ app/src/main/kotlin/example/poeaa/project/domain/Project.kt
@@ -0,0 +1,7 @@
+package example.poeaa.project.domain
+
+data class Project(
+        val id: ProjectId,
+        val name: String,
+        val cost: Long
+)
---
+++ app/src/main/kotlin/example/poeaa/project/domain/ProjectId.kt
@@ -0,0 +1,5 @@
+package example.poeaa.project.domain
+
+import java.util.UUID
+
+data class ProjectId(val value: UUID)
