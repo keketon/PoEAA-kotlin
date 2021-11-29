663c805c03e1ca5570065517899bede847b5fd3d
keketon <mas08rin10yabko@gmail.com>
2021-11-29 20:56:13 +0900

    add engineers to project

--- app/src/main/kotlin/example/poeaa/project/domain/Project.kt
+++ app/src/main/kotlin/example/poeaa/project/domain/Project.kt
@@ -1,7 +1,10 @@
 package example.poeaa.project.domain
 
+import example.poeaa.engineer.domain.EngineerId
+
 data class Project(
         val id: ProjectId,
         val name: String,
-        val cost: Long
+        val cost: Long,
+        val engineers: Set<EngineerId>
 )
